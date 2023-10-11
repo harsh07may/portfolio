c103a55f51b40c75da1c37c9e390af9064a38e9f
Harsh <myemail@example.com>
2023-10-11 14:41:27 +0530

    testing env

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
+const name = process.env.NAME;
+
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
+    <footer className="px-4 mb-10 text-center text-gray-500">
+      <small className="block mb-2 text-xs">
         &copy; 2023 Harsh. All rights reserved.
       </small>
       <p className="text-xs">
         This website was built with React, Next.js, TypeScript, Tailwind CSS &
         Framer Motion.
       </p>
+      <p>{name}</p>
     </footer>
   );
 }
